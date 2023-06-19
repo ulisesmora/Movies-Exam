b3107eb198855da9f87605947fe5a5c42069a34d
ulises mora <ulisesmora3@gmail.cm>
2023-06-19 15:04:53 -0600

    docker final

--- Movies-Exam/Migrations/20230619194802_Initial.Designer.cs
+++ Movies-Exam/Migrations/20230619194802_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movies_Exam.Persistence;
 
@@ -11,9 +12,11 @@ using Movies_Exam.Persistence;
 namespace Movies_Exam.Migrations
 {
     [DbContext(typeof(MoviesContext))]
-    partial class MoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20230619194802_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
